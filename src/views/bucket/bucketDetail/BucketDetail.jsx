0cc341f6d7171a21de8856901e07f855d3a31f6a
Canals233 <1552043941@qq.com>
2023-03-23 13:34:00 +0800

    change detail menu

--- src/views/bucket/bucketDetail/BucketDetail.jsx
+++ src/views/bucket/bucketDetail/BucketDetail.jsx
@@ -2,12 +2,11 @@ import { Layout, Menu } from "antd";
 import {
 	AppstoreOutlined,
 	MailOutlined,
-	SettingOutlined,
+	ArrowLeftOutlined 
 } from "@ant-design/icons";
 import React from "react";
 import { useSelector } from "react-redux";
 import { selectBucketListByName } from "@/redux/modules/bucketSlice";
-import Sider from "antd/lib/layout/Sider";
 
 function getItem(label, key, icon, children, type) {
 	return {
@@ -19,59 +18,39 @@ function getItem(label, key, icon, children, type) {
 	};
 }
 const items = [
-	getItem("Navigation One", "sub1", <MailOutlined />, [
-		getItem(
-			"Item 1",
-			"g1",
-			null,
-			[getItem("Option 1", "1"), getItem("Option 2", "2")],
-			"group"
-		),
-		getItem(
-			"Item 2",
-			"g2",
-			null,
-			[getItem("Option 3", "3"), getItem("Option 4", "4")],
-			"group"
-		),
-	]),
-	getItem("Navigation Two", "sub2", <AppstoreOutlined />, [
-		getItem("Option 5", "5"),
-		getItem("Option 6", "6"),
-		getItem("Submenu", "sub3", null, [
-			getItem("Option 7", "7"),
-			getItem("Option 8", "8"),
-		]),
-	]),
-	getItem("Navigation Three", "sub4", <SettingOutlined />, [
-		getItem("Option 9", "9"),
-		getItem("Option 10", "10"),
-		getItem("Option 11", "11"),
-		getItem("Option 12", "12"),
+	getItem("文件列表", "file", <MailOutlined />),
+	getItem("基本配置", "config", <AppstoreOutlined />, [
+		getItem("标签管理", "tagsConfig"),
+		getItem("权限管理", "visiableConfig"),
 	]),
 ];
 
 const BucketDetail = ({ bucketName }) => {
 	const currentBucket = useSelector(selectBucketListByName(bucketName));
 	console.log(currentBucket);
+	const onClick = (e) => {
+		console.log("click ", e);
+	};
 	return (
 		<>
-			< >
-				<div style={{
-                    width:200,
-                    height:'100%',
-                    backgroundColor:'white'
-                    
-                }}>
+			
+				<div
+					style={{
+						width: 180,
+						height: "100%",
+						backgroundColor: "white",
+					}}
+				>
+                    <ArrowLeftOutlined />
 					<Menu
 						defaultSelectedKeys={["1"]}
 						defaultOpenKeys={["sub1"]}
 						mode="inline"
 						items={items}
+						onClick={onClick}
 					/>
 				</div>
-                
-			</>
+			
 		</>
 	);
 };
